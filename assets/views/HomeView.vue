93c3dba356ce9ea1df53851468d1696516e76547
Geronimo Pröbster <gerproe@googlemail.com>
2022-07-09 19:27:58 +0200

    [TASK] Initial commit

---
+++ assets/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "@/components/HelloWorld";
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/user/create", name="create_user")
+     */
+    public function createUser(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new User();
+        $product->setName('Gero');
+        $product->setMail('mymail@lol.de');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new user with id '.$product->getId());
+    }
+}
